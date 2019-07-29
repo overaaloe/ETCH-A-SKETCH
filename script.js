dbbc0e1589048ff6c9b7f1bcb5118eaa73c3fc54
overaaloe <overaaloe@gmail.com>
2019-07-29 15:20:09 +0800

    2019.07.29

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 var n = 16;
-box = div.querySelector('.box');
-box.setAttribute('display', 'flex');
-const content = document.createElement('div');
-content.classList.add('content');
-for (i=0;i<n;i++){
-	for (j=i;j<n;j++){
-		content.textContent = 'Hello';
+const box = document.querySelector('#box');
+box.setAttribute('style', 'display:flex; flex-wrap: wrap; justify-content: space-evenly');
+
+for(i=0;i<n;i++){
+	for(j=0;j<n;j++){
+		const container = document.createElement('div');
+		container.classList.add('container'); 
+		container.setAttribute('style', 'background-color:aquamarine; width:10px; height:10px;border:1px solid black;');    
+		box.appendChild(container);
 	}
 }
-box.appendChild(content)
+var grid = document.querySelector('.container');
+console.log(grid)
+grid.addEventListener('mouseover', function( event ) {   
+	// highlight the mouseover target
+	event.target.style.color = "orange";
+});
